dd2be36fd9e76967163203c99f8aa806cfa60589
Sebastian <sebaramirezx2010@gmail.com>
2022-10-04 20:22:12 -0300

    actualizado

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ var industrial = document.getElementById("industrial");
 
 var zonaDom = document.getElementById("zonaDom");
 var kWh = document.getElementById("kWhMes");
-        
+var centro = document.getElementById("centro");
+
 function validar(){
     if (residencial.checked==true){  // pregunto si la opcion residencial fue presionada y muestro el alert
         alert("Ha seleccionado Residencial");
+        residencial = 0.21;
     }
     else if(industrial.checked==true){
        alert("Ha seleccionado industrial");
+       industrial = 0.27;
     }
     else{       // sino selecciono ninguna de las dos opciones le muestro este alert
         alert("Por favor seleccione una opcion de Tipo de usuario");
@@ -23,8 +26,19 @@ function validar(){
             
     }
     else{
-            alert("Perfecto");
+        if(residencial.checked==true){
+            switch (zonaDom) {
+                case centro:
+                   resultado = 102+(kWh.value*5.80)*(1+0.20);
+                   alert()
+                   
+                    break;
             
+                default:
+                    break;
+            }
+        }
+        
     }
 
 
